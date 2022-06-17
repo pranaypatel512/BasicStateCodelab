bbf752c9071f40ef027ab650377467b1fd918058
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-06-17 13:17:16 +0530

    `rememberSaveable` use cases

--- app/src/main/java/com/pranay/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/pranay/basicstatecodelab/WaterCounter.kt
@@ -9,7 +9,11 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,36 +26,21 @@ fun WellnessCounter(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(taskName = "Have you taken your 15 minute walk today?", onClose = {
-                    showTask = false
-                })
-            }
             Text(
                 text = "You have had $count glasses.",
                 modifier = modifier.padding(16.dp)
             )
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text(
-                    text = "Add one",
-                )
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = modifier.padding(start = 8.dp)
-            ) {
-                Text(
-                    text = "Clear water count",
-                )
-            }
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text(
+                text = "Add one",
+            )
         }
     }
 }
