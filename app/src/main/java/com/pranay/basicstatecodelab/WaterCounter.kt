6bb421b3273974e7fe485fa0d8a72f924d6b68e6
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-06-17 13:00:08 +0530

    updated code for state driven ui

--- app/src/main/java/com/pranay/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/pranay/basicstatecodelab/WaterCounter.kt
@@ -17,11 +17,17 @@ fun WellnessCounter(modifier: Modifier = Modifier) {
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        Text(
-            text = "You have had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text(
+                text = "You have had $count glasses.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text(
                 text = "Add one",
             )
